998b35a293ef0c0d10cb6f6171e64f37b57a55ce
liho-o <67860443+liho-o@users.noreply.github.com>
2024-02-26 21:29:10 +0700

    Second commit

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ lang_lbr = {
     '1': {
         '1': 'Выберите металл',
         '2': 'Введите количество грамм'
+    },
+    '2': {
+        '1': 'Choose metal',
+        '2': 'Enter how many metals'
     }
 }
-def choes(lang):
-    first_mes = ''
-    metal = input(f'{lang_lbr[lang][1]}')
 
 
 if __name__ == '__main__':
     lang = input('Выберите язык: | Chose your language: \n1 - RU | 2 - EN')
-    choes(lang)
+    metal = input(f'{lang_lbr[lang][1]}')
+    neto = input(f'{lang_lbr[lang][2]}')
+
 
