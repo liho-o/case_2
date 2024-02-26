bf8a5603adb2059950b24c1b51898e05d55ab14d
liho-o <67860443+liho-o@users.noreply.github.com>
2024-02-26 22:23:07 +0700

    Main commit

---
+++ ferum.py
@@ -0,0 +1,15 @@
+alloys = {
+    'Steel': {
+        'fe': 0.985,
+        'c': 0.015
+    },
+    'Cast_iron': {
+        'fe': 0.95,
+        'c': 0.05
+    },
+    'Stainless_steel': {
+        'fe': 0.7,
+        'cr': 0.2,
+        'ni': 0.1
+    }
+}
--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import lang_templates
+import ferum
 
 lang_lbr = lang_templates.lang_lbr
 
 
 def previos():
-    lang = input('Выберите язык: | Chose your language: \n1 - RU | 2 - EN')
+    lang = input('Выберите язык: | Chose your language: \n1 - RU | 2 - EN\n')
     metal = input(f'{lang_lbr[lang][1]}')
     neto = input(f'{lang_lbr[lang][2]}')
     return lang, metal, neto
 
 
 def main(lang, metal, neto):
-    pass
+    # metal = 'fe'
+    # neto = 100
+    for alloy in ferum.alloys:
+        print(alloy)
+        local_alloy = ferum.alloys[alloy]
+        local_alloy_neto = local_alloy[metal]
+        total_alloy_neto = neto / local_alloy_neto
+        for matireal in ferum.alloys[alloy]:
+            if metal == matireal:
+                continue
+            metal_koef = ferum.alloys[alloy][matireal]
+            request = total_alloy_neto * metal_koef
+            print(matireal, ' --- ', request)
+
+        print(total_alloy_neto, end='\n\n')
 
 
 if __name__ == '__main__':
     lang, metal, neto = previos()
     main(lang, metal, neto)
-
 
 
 
