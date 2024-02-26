5d273a8e8c82bf9e23c579643786825e78c3e2f4
liho-o <67860443+liho-o@users.noreply.github.com>
2024-02-26 21:43:18 +0700

    Third commit

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-lang_lbr = {
-    '1': {
-        '1': 'Выберите металл',
-        '2': 'Введите количество грамм'
-    },
-    '2': {
-        '1': 'Choose metal',
-        '2': 'Enter how many metals'
-    }
-}
+import lang_templates
+
+lang_lbr = lang_templates.lang_lbr
 
 
-if __name__ == '__main__':
+def previos():
     lang = input('Выберите язык: | Chose your language: \n1 - RU | 2 - EN')
     metal = input(f'{lang_lbr[lang][1]}')
     neto = input(f'{lang_lbr[lang][2]}')
+    return lang, metal, neto
+
+
+def main(lang, metal, neto):
+    pass
+
+
+if __name__ == '__main__':
+    lang, metal, neto = previos()
+    main(lang, metal, neto)
+
+
 
 
