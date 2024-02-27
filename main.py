c5a136a41456363d65e8e2198846883eaccec446
alisa <alisa.solodovnik@gmail.com>
2024-02-27 19:16:45 +0700

    add changes

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Part of case-study #2: Alloys
+# Developers: Solodovnik A., Kabanova M., Soknyshev D., Kim A.
+#
+
 import lang_templates
 import ferum
 
@@ -5,25 +9,26 @@ lang_lbr = lang_templates.lang_lbr
 
 
 def previos():
-    lang = str(input('Выберите язык: | Chose your language: \n1 - RU | 2 - EN\n'))
-    metal = input(f'{lang_lbr[lang]["1"]}')
-    neto = int(input(f'{lang_lbr[lang]["2"]}'))
+    lang = input('Выберите язык: | Chose your language: \n1 - RU | 2 - EN\n')
+    metal = input(f'{lang_lbr[lang][1]}')
+    neto = input(f'{lang_lbr[lang][2]}')
     return lang, metal, neto
 
 
 def main(lang, metal, neto):
+    # metal = 'fe'
+    # neto = 100
     for alloy in ferum.alloys:
         print(alloy)
         local_alloy = ferum.alloys[alloy]
         local_alloy_neto = local_alloy[metal]
-        print(neto, local_alloy_neto)
         total_alloy_neto = neto / local_alloy_neto
-        for matireal in ferum.alloys[alloy]:
-            if metal == matireal:
+        for material in ferum.alloys[alloy]:
+            if metal == material:
                 continue
-            metal_koef = ferum.alloys[alloy][matireal]
+            metal_koef = ferum.alloys[alloy][material]
             request = total_alloy_neto * metal_koef
-            print(matireal, ' --- ', request)
+            print(material, ' --- ', request)
 
         print(total_alloy_neto, end='\n\n')
 
@@ -31,7 +36,3 @@ def main(lang, metal, neto):
 if __name__ == '__main__':
     lang, metal, neto = previos()
     main(lang, metal, neto)
-
-
-
-
