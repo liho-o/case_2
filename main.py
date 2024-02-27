c5ecea04a780c7bb98a81b0c264e01f6bd54b5ee
liho-o <67860443+liho-o@users.noreply.github.com>
2024-02-27 19:15:45 +0700

    NEW commit

--- main.py
+++ main.py
@@ -7,17 +7,16 @@ lang_lbr = lang_templates.lang_lbr
 def previos():
     lang = str(input('Выберите язык: | Chose your language: \n1 - RU | 2 - EN\n'))
     metal = input(f'{lang_lbr[lang]["1"]}')
-    neto = input(f'{lang_lbr[0][lang]["2"]}')
+    neto = int(input(f'{lang_lbr[lang]["2"]}'))
     return lang, metal, neto
 
 
 def main(lang, metal, neto):
-    # metal = 'fe'
-    # neto = 100
     for alloy in ferum.alloys:
         print(alloy)
         local_alloy = ferum.alloys[alloy]
         local_alloy_neto = local_alloy[metal]
+        print(neto, local_alloy_neto)
         total_alloy_neto = neto / local_alloy_neto
         for matireal in ferum.alloys[alloy]:
             if metal == matireal:
