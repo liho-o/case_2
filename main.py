d7bba0d45ff111fdde516dab1c81728093b01cd4
liho-o <daniil200120@mail.ru>
2024-02-28 11:20:46 +0700

    Ready

--- main.py
+++ main.py
@@ -11,13 +11,36 @@ lang_lbr = lang_templates.lang_lbr  # Rename file
 
 
 def previous():  # Requesting user input
+    """
+    First function.
+    :return: lang, metal, neto
+    """
+
     lang = str(input('Выберите язык: | Chose your language: \n1 - RU | 2 - EN\n'))
     metal = input(f'{lang_lbr[lang]["1"]}')
     neto = int(input(f'{lang_lbr[lang]["2"]}'))
     return lang, metal, neto
 
 
-def main_calculations(lang, metal, neto, main_dict):
+def main(lang, metal, neto):
+    """
+    Main function.
+    :return: None
+    """
+
+    if metal == 'fe':
+        main_dict = ferum
+
+    elif metal == 'cu':
+        main_dict = cuprum
+
+    elif metal == 'ni':
+        main_dict = nickel
+
+    else:
+        print(f'{lang_lbr[lang]["0"]}')
+        return
+
     for alloy in main_dict.alloys[lang]:
         local_alloy = main_dict.alloys[lang][alloy]  # Alloys for this metal
         local_alloy_neto = local_alloy[metal]  # Adresses percentage of metal for alloy
@@ -32,23 +55,6 @@ def main_calculations(lang, metal, neto, main_dict):
             metal_koef = main_dict.alloys[lang][alloy][material]  # Adresses percentage of missing for alloy
             request = round(total_alloy_neto * metal_koef, 2)  # Counting missing elements
             print(material, ' --- ', request, end='\n')
-
-
-def main(lang, metal, neto):
-    if metal == 'fe':
-        main_dict = ferum
-
-    elif metal == 'cu':
-        main_dict = cuprum
-
-    elif metal == 'ni':
-        main_dict = nickel
-
-    else:
-        print(f'{lang_lbr[lang]["0"]}')
-        return
-
-    main_calculations(lang, metal, neto, main_dict)
 
 
 if __name__ == '__main__':  # Run only if this file is active
